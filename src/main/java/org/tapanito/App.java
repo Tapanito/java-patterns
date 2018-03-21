e45585de652e4015dd077ccf79d8ccde9a88ac31
Tapanito <vtumas@gmail.com>
2018-03-21 19:20:37 +0000

    initial commit

---
+++ src/main/java/org/tapanito/App.java
@@ -0,0 +1,13 @@
+package org.tapanito;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
